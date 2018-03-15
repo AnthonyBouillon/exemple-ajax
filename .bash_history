72d51a42d40ff125b56a2534b281b89e58964a17
Anthony <anthony_bouillon@outlook.fr>
2018-03-15 10:07:46 +0100

    Inscription en ajax

---
+++ .bash_history
@@ -0,0 +1,34 @@
+cd ~
+pwd
+ls
+ls -al
+ls
+composer
+clear
+exit
+composer install jquery
+composer require jquery
+pwd
+cd 
+clear
+composer require jquery
+man composer
+composer require jquery 3.3.1
+composer require jquery --3.3.1
+composer require components/jquery
+clear
+ls
+ls vendor/
+ls vendor/components/
+exit
+cd 
+ls
+zip ajax *
+sudo apt install zip
+sudo -s
+exit
+zip ajax *
+cd 
+zip ajax *
+ls
+exit
---
+++ assets/js/checkUnique.js
@@ -0,0 +1,35 @@
+
+function checkLoginUnique() {
+    $.post(
+            '../../controllers/indexController.php',
+            {
+                checkLogin: $('#login').val()
+            },
+            function (checkLoginResult) {
+                if (checkLoginResult == 1) {
+                    $('#errorCheckLoginUnique').css('display', 'block');
+                }else{
+                    $('#errorCheckLoginUnique').css('display', 'none');
+                }
+            },
+            'JSON'
+            )
+}
+
+function checkMailUnique() {
+    $.post(
+            '../../controllers/indexController.php',
+            {
+                checkMail: $('#mail').val()
+            },
+            function (checkMailResult) {
+                if (checkMailResult == 1) {
+                    $('#errorCheckMailUnique').css('display', 'block');
+                }else{
+                    $('#errorCheckMailUnique').css('display', 'none');
+                }
+            },
+            'JSON'
+            )
+}
+
